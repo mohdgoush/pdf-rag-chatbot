1283bcef336672f113c84f3b64b443de5a6f98b4
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-02 18:42:29 +0530

    Added Sementic Retrieval

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from src.pdf_reader import extract_text_from_pdf
 from src.chunking import chunk_text
-from src.embeddings import create_embeddings
+from src.embeddings import create_embeddings, model
 from src.vector_store import create_vector_store
+from src.retriever import retrieve_relevant_chunks
 
 
 pdf_path = "data/IET_lucknow_guideline_pdf.pdf"
@@ -14,6 +15,20 @@ embeddings = create_embeddings(chunks)
 
 vector_store = create_vector_store(embeddings)
 
-print("FAISS Vector Store Created Successfully!")
+query = input("Enter your question: ")
 
-print(f"Total vectors stored: {vector_store.ntotal}")
+retrieved_chunks = retrieve_relevant_chunks(
+    query,
+    model,
+    vector_store,
+    chunks
+)
+
+print("\nRetrieved Chunks:\n")
+
+for i, chunk in enumerate(retrieved_chunks):
+
+    print(f"\nChunk {i+1}:\n")
+
+    print(chunk)
+    print("-" * 80)
