c474c663589c138064e8744cf1b112734fac475e
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-02 18:52:56 +0530

    Added Conversational Memory to the RAG system

--- app.py
+++ app.py
@@ -3,32 +3,67 @@ from src.chunking import chunk_text
 from src.embeddings import create_embeddings, model
 from src.vector_store import create_vector_store
 from src.retriever import retrieve_relevant_chunks
+from src.rag_pipeline import generate_response
 
 
 pdf_path = "data/IET_lucknow_guideline_pdf.pdf"
 
+print("\nReading PDF...")
+
 text = extract_text_from_pdf(pdf_path)
+
+print("Chunking text...")
 
 chunks = chunk_text(text)
 
+print("Creating embeddings...")
+
 embeddings = create_embeddings(chunks)
 
+print("Creating vector store...")
+
 vector_store = create_vector_store(embeddings)
+chat_history = []
 
-query = input("Enter your question: ")
+print("\nRAG Chatbot Ready!")
 
-retrieved_chunks = retrieve_relevant_chunks(
-    query,
-    model,
-    vector_store,
-    chunks
-)
+while True:
 
-print("\nRetrieved Chunks:\n")
+    query = input("\nAsk a question: ")
 
-for i, chunk in enumerate(retrieved_chunks):
+    if query.lower() == "exit":
+        print("Goodbye!")
+        break
 
-    print(f"\nChunk {i+1}:\n")
+    retrieved_chunks = retrieve_relevant_chunks(
+        query,
+        model,
+        vector_store,
+        chunks
+    )
 
-    print(chunk)
-    print("-" * 80)
+    response = generate_response(
+        query,
+        retrieved_chunks,
+        chat_history
+    )
+
+    print("\nAnswer:\n")
+
+    print(response)
+
+    # Save user message
+    chat_history.append(
+        {
+            "role": "user",
+            "content": query
+        }
+    )
+
+    # Save assistant response
+    chat_history.append(
+        {
+            "role": "assistant",
+            "content": response
+        }
+    )
---
+++ src/rag_pipeline.py
@@ -0,0 +1,56 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+
+def generate_response(query, retrieved_chunks, chat_history):
+
+    context = "\n\n".join(retrieved_chunks)
+
+    history = ""
+
+    for message in chat_history:
+
+        role = message["role"]
+        content = message["content"]
+
+        history += f"{role}: {content}\n"
+
+    prompt = f"""
+You are a helpful AI assistant.
+
+Use the provided context and conversation history
+to answer the user's question.
+
+Conversation History:
+{history}
+
+Context:
+{context}
+
+Question:
+{query}
+"""
+
+    response = client.chat.completions.create(
+
+        model="llama-3.3-70b-versatile",
+
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+
+        temperature=0.3,
+        max_tokens=1024
+    )
+
+    return response.choices[0].message.content
