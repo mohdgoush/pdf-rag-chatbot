e8984f15a91e5a6d7364da7b747a4de1686b834f
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-02 18:29:34 +0530

    Implemented embedding generation

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from src.pdf_reader import extract_text_from_pdf
 from src.chunking import chunk_text
+from src.embeddings import create_embeddings
+
 
 pdf_path = "data/IET_lucknow_guideline_pdf.pdf"
+
 text = extract_text_from_pdf(pdf_path)
+
 chunks = chunk_text(text)
+
+embeddings = create_embeddings(chunks)
+
 print(f"Total Chunks: {len(chunks)}")
-print("\nFIRST CHUNK:\n")
-print(chunks[0])
+
+print(f"Embedding Shape: {embeddings.shape}")
+
+print("\nFirst Embedding Vector:\n")
+
+print(embeddings[0])
