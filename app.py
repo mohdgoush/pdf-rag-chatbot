d9b85d38c82cb309d2b216d7b4ec69658fa42e39
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-02 18:22:57 +0530

    Implemented text Chuncking

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from src.pdf_reader import extract_text_from_pdf
+from src.chunking import chunk_text
+
 pdf_path = "data/IET_lucknow_guideline_pdf.pdf"
 text = extract_text_from_pdf(pdf_path)
-print(text[:1000])
+chunks = chunk_text(text)
+print(f"Total Chunks: {len(chunks)}")
+print("\nFIRST CHUNK:\n")
+print(chunks[0])
